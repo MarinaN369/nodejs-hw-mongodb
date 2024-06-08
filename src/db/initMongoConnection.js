974ec802330c02ca5e9db21e5f38b748bc821c57
MarinaN369 <143936204+MarinaN369@users.noreply.github.com>
2024-06-08 16:56:49 +0300

    update

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -1,16 +1,18 @@
 import mongoose from 'mongoose';
 import {env} from '../utils/env.js';
 
+
 // import dotenv from 'dotenv';
 // dotenv.config();
 
+
 export const initMongoConnection = async() => {
     try {
-    const user = env('MONGODB_USER');
-    const psw = env('MONGODB_PASSWORD');
-    const url = env('MONGODB_URL');
-    const db = env('MONGODB_DB');
-    await mongoose.connect(`mongodb+srv://${user}:${psw}@${url}/${db}?retryWrites=true&w=majority&appName=Cluster0`,);
+        const user = env('MONGODB_USER');
+        const pwd = env('MONGODB_PASSWORD');
+        const url = env('MONGODB_URL');
+        const db = env('MONGODB_DB');
+    await mongoose.connect(`mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority&appName=Cluster0`,);
 
     console.log('Mongo connection successfully established!');}
     catch (error) {
