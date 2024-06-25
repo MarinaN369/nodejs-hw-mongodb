c7bfd6d6d2c31db48f5421e78269054c08027864
MarinaN369 <143936204+MarinaN369@users.noreply.github.com>
2024-06-26 00:40:20 +0300

    hw4

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -1,1 +1,19 @@
 import Joi from 'joi';
+
+export const createContactSchema = Joi.object({
+    name: Joi.string()
+    .min(3)
+    .max(20)
+    .required(),
+    email: Joi.string()
+    .email()
+    .required(),
+    phoneNumber: Joi.string()
+    .min(3)
+    .max(20)
+    .required()
+    .messages({ 'string.base': 'PhoneNumber should be a string' }),
+  isFavourite: Joi.boolean(),
+  contactType: Joi.string()
+  .valid('work', 'home', 'personal').required(),
+  });
