c214c27b387e7ce7e106d4866aeb5b50a2fd7218
MarinaN369 <143936204+MarinaN369@users.noreply.github.com>
2024-06-28 12:53:54 +0300

    hw3

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -13,6 +13,7 @@ export const getContactsController = async(req, res) => {
     const contacts = await getAllContacts();
 
     res.status(200).json({
+        status: 200,
         message: 'Successfully found contact!',
         data: contacts,
     });
@@ -28,6 +29,7 @@ export const getContactByIdController= async(req, res, next) => {
     }
 
     res.status(200).json({
+        status: 200,
         message: `Successfully found contact with id ${contactId}!`,
         data: contact,
     });
@@ -40,7 +42,7 @@ export const createContactController = async(req, res) => {
     res.status(201).json({
         status: 201,
         message: 'Successfully created a contact!',
-        data: {...contact.toObject(), __v: undefined},
+        data: contact,
     });
 };
 
