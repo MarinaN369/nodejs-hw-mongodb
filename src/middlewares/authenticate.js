9cb2a994a221d913c5454010dbaf469a7b9ebce9
MarinaN369 <143936204+MarinaN369@users.noreply.github.com>
2024-07-06 00:25:54 +0300

    hw5

--- src/middlewares/authenticate.js
+++ src/middlewares/authenticate.js
@@ -11,7 +11,6 @@ if(!authHeader) {
     return;
 }
 
-
 const bearer = authHeader.split(' ')[0];
 const token = authHeader.split(' ')[1];
 
--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -8,17 +8,16 @@ import { Router } from "express";
     import {validateBody} from '../middlewares/validateBody.js';
     import {createContactSchema, updateContactSchema} from '../validation/contacts.js';
  import {ctrlWrapper} from '../utils/ctrlWrapper.js';
- import  isValidId from "../middlewares/isValidId.js";
+//  import  isValidId from "../middlewares/isValidId.js";
  import { authenticate } from '../middlewares/authenticate.js';
 
 
 const router = Router();
 
 router.use(authenticate);
-router.use('/contacts/:contactId', isValidId('contactId'));
 router.get('/', ctrlWrapper(getContactsController));
 router.get('/:contactId', ctrlWrapper(getContactByIdController));
-router.post('/', validateBody(createContactSchema), ctrlWrapper(createContactController));
+router.post('', validateBody(createContactSchema), ctrlWrapper(createContactController));
 router.patch('/:contactId',  validateBody(updateContactSchema), ctrlWrapper(patchContactController));
 router.delete('/:contactId', ctrlWrapper(deleteContactController));
 
