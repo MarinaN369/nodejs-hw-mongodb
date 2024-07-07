693bf8425010bb35ad8d0be22770e2dc5d683f9f
MarinaN369 <143936204+MarinaN369@users.noreply.github.com>
2024-07-07 14:12:13 +0300

    hw5

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -8,12 +8,12 @@ import { Router } from "express";
     import {validateBody} from '../middlewares/validateBody.js';
     import {createContactSchema, updateContactSchema} from '../validation/contacts.js';
  import {ctrlWrapper} from '../utils/ctrlWrapper.js';
-//  import  isValidId from "../middlewares/isValidId.js";
+ import  isValidId from "../middlewares/isValidId.js";
  import { authenticate } from '../middlewares/authenticate.js';
-
 
 const router = Router();
 
+router.use('/:contactId', isValidId('contactId'));
 router.use(authenticate);
 router.get('/', ctrlWrapper(getContactsController));
 router.get('/:contactId', ctrlWrapper(getContactByIdController));
