7ba4d1283f850cf87c8c7a9d9e4d62244b743a05
MarinaN369 <143936204+MarinaN369@users.noreply.github.com>
2024-07-13 16:45:10 +0300

    hw7

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -5,6 +5,7 @@ import {refreshUsersSession} from '../services/auth.js';
 import { requestResetToken } from '../services/auth.js';
 import { resetPassword } from '../services/auth.js';
 import { generateAuthUrl } from '../utils/googleOAuth2.js';
+import { loginOrSignupWithGoogle } from '../services/auth.js';
 
 export const registerUserController = async(req, res) => {
     const user = await registerUser(req.body);
@@ -105,6 +106,19 @@ export const resetPasswordController = async (req, res) => {
       message: 'Successfully get Google OAuth url!',
       data: {
         url,
+      },
+    });
+  };
+
+  export const loginWithGoogleController = async (req, res) => {
+    const session = await loginOrSignupWithGoogle(req.body.code);
+    setupSession(res, session);
+
+    res.json({
+      status: 200,
+      message: 'Successfully logged in via Google OAuth!',
+      data: {
+        accessToken: session.accessToken,
       },
     });
   };
