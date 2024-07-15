04f66b9d9c2b0932f87d36352105a880f66a3634
MarinaN369 <143936204+MarinaN369@users.noreply.github.com>
2024-07-15 08:03:18 +0300

    hw7

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -3,6 +3,7 @@ import { calculatePaginationData } from '../utils/calculatePaginationData.js';
 import { SORT_ORDER } from '../index.js';
 
 export const getAllContacts = async ({
+  userId,
   page,
   perPage,
   sortOrder = SORT_ORDER.ASC,
@@ -11,8 +12,8 @@ export const getAllContacts = async ({
   const limit = perPage;
   const skip = (page - 1) * perPage;
 
-  const contactsQuery = contactsCollection.find();
-  const contactsCount = await contactsCollection.find()
+  const contactsQuery = contactsCollection.find({userId});
+  const contactsCount = await contactsCollection.find({userId})
     .merge(contactsQuery)
     .countDocuments();
 
